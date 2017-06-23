501ab18a71c0b328d3954b0224e66b8e83a84b39
Eric Liu <eric__1234@outlook.com>
2017-06-22 22:24:48 -0400

    Create Fast Exponents.cpp

---
+++ Fast Exponents.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+	char _;
+#define ll long long
+using namespace std;
+int N;
+ll i;
+int main() {
+   scan(N);
+   for (int j=0; j<N; j++){
+    scan(i);
+    printf("%s\n", (i & (i - 1) ? "F" : "T"));
+   }
+}
