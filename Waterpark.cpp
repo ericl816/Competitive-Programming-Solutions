493518eabdbb6390945f9e82ad85bcedb886492b
Eric Liu <eric__1234@outlook.com>
2017-06-27 17:22:00 -0400

    Create Waterpark.cpp

---
+++ Waterpark.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+int x = 1;
+int y = 1;
+vector<int> v,w;
+vector<int> G[10000];
+int DP[10000];
+
+int f(int a) {
+	if(a == n) {
+		DP[n] = 1;
+		return 1;
+	}
+	else {
+		int z = 0;
+		for(int i=0; i<G[a].size(); i++) {
+			if(DP[G[a][i]] == -1) {
+				DP[G[a][i]] = f(G[a][i]);
+				z = z + DP[G[a][i]]; 
+			}
+			else 
+				z = z + DP[G[a][i]];
+		}
+		return z;	
+	}
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+	cin >> n;
+	while(x != 0 && y != 0) {
+		cin >> x >> y;
+		G[x].push_back(y);
+	}
+	for(int i=0; i<10000; i++) {
+		DP[i] = -1;
+	}
+	cout << f(1) << endl;
+}
