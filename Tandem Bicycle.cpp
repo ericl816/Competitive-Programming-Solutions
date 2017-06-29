538214f76798578cbb8186bbc8bf5c8c91f1ef6e
Eric Liu <eric__1234@outlook.com>
2017-06-28 20:39:12 -0400

    Create Tandem Bicycle.cpp

---
+++ Tandem Bicycle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int d[105], p[105], type, people, sum = 0;
+
+int main() {   
+    scanf("%d \n %d \n", &type, &people);
+    for (int i=0; i<people; i++) scanf("%d ", &d[i]);
+    for (int i=0; i<people; i++) scanf("%d ", &p[i]);
+    sort(d, d + people); 
+    sort(p, p + people);
+    if (type) {
+        for (int i=0; i<people; i++) sum += max(d[i], p[i]);
+    }
+    else {
+        for (int i=0; i<people; i++) sum += max(d[people - 1 - i], p[i]);
+    }
+    printf("%d\n", sum); 
