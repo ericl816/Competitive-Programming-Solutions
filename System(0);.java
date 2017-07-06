4ec2ad96d740d1b1671f2729934892806f88da69
Eric Liu <eric__1234@outlook.com>
2017-07-06 12:54:50 -0400

    Create System(0);.java

---
+++ System(0);.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.DecimalFormat;
+
+public class System0 {
+
+    static BufferedReader br;
+    static PrintWriter out;
+    static StringTokenizer st;
+    
+    static int a;
+    static double b, c;
+    
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+	out = new PrintWriter(new OutputStreamWriter(System.out));
+        DecimalFormat df = new DecimalFormat("#0.000000");
+        a = readInt(); b = readDouble(); c = readDouble();
+        System.out.println(df.format(-1/(b*a/c)));
+    }
+    
+        static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+
+}
