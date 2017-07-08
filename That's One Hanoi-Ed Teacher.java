f8707c606ce2bf2b54e547e38e921ee45c47a50c
Eric Liu <eric__1234@outlook.com>
2017-07-07 20:07:06 -0400

    Update That's One Hanoi-Ed Teacher.java

--- That's One Hanoi-Ed Teacher.java
+++ That's One Hanoi-Ed Teacher.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 import java.util.*;
 
 public class ThatsOneHanoiEdTeacher {
