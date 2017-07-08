e0183b21818aa0ff30cdd84be27a2b210f664b92
Eric Liu <eric__1234@outlook.com>
2017-07-07 20:23:13 -0400

    Update That's One Hanoi-Ed Teacher.java

--- That's One Hanoi-Ed Teacher.java
+++ That's One Hanoi-Ed Teacher.java
@@ -1,41 +1,149 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
+import java.io.*;
 import java.util.*;
 
-public class ThatsOneHanoiEdTeacher {
+public class Main {
 
-    static BufferedReader br;
-    static PrintWriter out;
-    static StringTokenizer st;
-    
-    static int MAXN = 1001;
-    static int a [] = new int [MAXN];
-    static int c [] = new int [3];
-    static boolean valid = true; 
-    static int curr, next;
-    static int peg1 = 1;
-    static int peg2 = 0;
-    static long cnt = 0;
+    private static Main o = new Main();
+
+    class Reader {
+    final private int BUFFER_SIZE = 1 << 16;
+    private DataInputStream din;
+    private byte[] buffer;
+    private int bufferPointer, bytesRead;
+    private int http;
+
+    public Reader() {
+        din = new DataInputStream(System.in);
+        buffer = new byte[BUFFER_SIZE];
+        bufferPointer = bytesRead = 0;
+    }
+
+    public Reader(String file_name) throws IOException {
+        din = new DataInputStream(new FileInputStream(file_name));
+        buffer = new byte[BUFFER_SIZE];
+        bufferPointer = bytesRead = 0;
+    }
+
+    public String readLine() throws IOException {
+        byte[] buf = new byte[64]; // line length
+        int cnt = 0, c;
+        while ((c = read()) != -1) {
+            if (c == '\n') {
+                break;
+            }
+            buf[cnt++] = (byte) c;
+        }
+        return new String(buf, 0, cnt);
+    }
+
+    public int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') {
+            c = read();
+        }
+        boolean neg = (c == '-');
+        if (neg) {
+            c = read();
+        }
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+
+        if (neg) {
+            return -ret;
+        }
+        return ret;
+    }
+
+    public long nextLong() throws IOException {
+        long ret = 0;
+        byte c = read();
+        while (c <= ' ') {
+            c = read();
+        }
+        boolean neg = (c == '-');
+        if (neg) {
+            c = read();
+        }
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) {
+            return -ret;
+        }
+        return ret;
+    }
+
+    public double nextDouble() throws IOException {
+        double ret = 0, div = 1;
+        byte c = read();
+        while (c <= ' ') {
+            c = read();
+        }
+        boolean neg = (c == '-');
+        if (neg) {
+            c = read();
+        }
+
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+
+        if (c == '.') {
+            while ((c = read()) >= '0' && c <= '9') {
+                ret += (c - '0') / (div *= 10);
+            }
+        }
+
+        if (neg) {
+            return -ret;
+        }
+        return ret;
+    }
+
+    private void fillBuffer() throws IOException {
+
+        bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+        if (bytesRead == -1) {
+            buffer[0] = -1;
+        }
+    }
+
+    private byte read() throws IOException {
+        if (bufferPointer == bytesRead) {
+            fillBuffer();
+        }
+        return buffer[bufferPointer++];
+    }
+
+    public void close() throws IOException {
+        if (din == null) {
+            return;
+        }
+        din.close();
+    }
+}
+    public static int MAXN = 1010;
+    public static int a [] = new int [MAXN];
+    public static int c [] = new int [3];
+    public static boolean valid = true; 
+    public static int curr, next;
+    public static int peg1 = 1, peg2 = 0;
+    public static long cnt = 0;
 		
     public static void main(String[] args) throws IOException {
-      br = new BufferedReader(new InputStreamReader(System.in));
-      out = new PrintWriter(new OutputStreamWriter(System.out));
-	    for(int i = 0; i < 3; i++) {
-		  c[i] = readInt();
+      Reader r = o.new Reader();
+	    for(int i=0; i<3; i++) {
+		  c[i] = r.nextInt();
 		  curr = MAXN;
-		  
-		for(int j = 0; j < c[i]; j++) {
-		     next = readInt();
+		for(int j=0; j<c[i]; j++) {
+		     next = r.nextInt();
 		    if (curr <= next) valid = false;
-		     a[next] = i;
+		      a[next] = i;
 		      curr = next;
 	    	}
 	    }
-
 	   for (int i=c[0] + c[1] + c[2]; i>0; i--) {
 		if (a[i] == peg1) valid = false;
 		else { 
@@ -47,35 +155,4 @@ public class ThatsOneHanoiEdTeacher {
 	    }
 	    System.out.println((valid ? cnt : "No"));
     }
-
-                static String next () throws IOException {
-			while (st == null || !st.hasMoreTokens())
-				st = new StringTokenizer(br.readLine().trim());
-			return st.nextToken();
-		}
-
-
-		static long readLong () throws IOException {
-			return Long.parseLong(next());
-		}
-
-
-		static int readInt () throws IOException {
-			return Integer.parseInt(next());
-		}
-
-
-		static double readDouble () throws IOException {
-			return Double.parseDouble(next());
-		}
-
-
-		static char readCharacter () throws IOException {
-			return next().charAt(0);
-		}
-
-
-		static String readLine () throws IOException {
-			return br.readLine().trim();
-		}
-	}
+}
