7b03c39142179333b46dd1485f04faebd7e4e6c4
Eric Liu <eric__1234@outlook.com>
2017-07-25 22:12:51 -0400

    Create Zero that Out.py

---
+++ Zero that Out.py
@@ -0,0 +1,15 @@
+import sys
+raw_input = sys.stdin.readline
+k = int(raw_input())
+
+list = []
+for i in range(k):
+    n = int(raw_input())
+    if n > 0:
+        list.append(n)
+    else:
+        list.pop()
+sum = 0
+for i in range(len(list)):
+    sum = sum + list[i]
+print sum
