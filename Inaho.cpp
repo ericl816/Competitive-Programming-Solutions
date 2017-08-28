5a6dfd7fe3188f8d9cd85d65ddb33a761de2e8f2
Eric Liu <eric__1234@outlook.com>
2017-08-28 19:21:50 -0400

    Update Inaho.cpp

--- Inaho.cpp
+++ Inaho.cpp
@@ -1,32 +1,37 @@
 #include <bits/stdc++.h>
 #include "inaho.h"
+#define MAXN 500010
+#define INF 0x3f3f3f3f
+#define pii pair<int, int>
+#define f first
+#define s second
+#define node pair<pii, int>
+#define p push
+#define mp make_pair
 using namespace std;
 
-const int MAXN = 500010;
-int lead[MAXN];
-int size[MAXN];
-int NN;
-typedef pair<int, int> pii;
-typedef pair<pii, int> node;
+int lead[MAXN], size[MAXN];
 stack <node> al;
 node tmp;
+
 void Init(int N) {
-    NN = N;
-    for (int i = 0 ; i <= N; i++) {
+    for (int i=0; i<=N; i++) {
         lead[i] = i;
         size[i] = 1;
     }
 }
+
 int Find(int i) {
-    return (lead[i]==i)? i : Find(lead[i]);
+    return (lead[i] == i)? i : Find(lead[i]);
 }
+
 void AddEdge(int U, int V) {
     int ru = Find(U), rv = Find(V);
     if(ru == rv) {
-        al.push(make_pair(make_pair(ru, rv), 0));
+        al.p(mp(mp(ru, rv), 0));
         return;
     }
-    al.push(make_pair(make_pair(ru, rv), 1));
+    al.p(mp(mp(ru, rv), 1));
     if (size[ru] > size[rv]) {
         size[ru] += size[rv];
         lead[rv] = lead[ru];
@@ -36,20 +41,21 @@ void AddEdge(int U, int V) {
         lead[ru] = lead[rv];
     }
 }
+
 void RemoveLastEdge() {
-    tmp = al.top();
-    al.pop();
-    if (!tmp.second) return;
-    pii fin = tmp.first;
-    if (lead[fin.first] == fin.first) {
-        lead[fin.second] = fin.second;
-        size[fin.first] -= size[fin.second];
+    tmp = al.top(); al.pop();
+    if (!tmp.s) return;
+    pii next = tmp.f;
+    if (lead[next.f] == next.f) {
+        lead[next.s] = next.s;
+        size[next.f] -= size[next.s];
     }
     else {
-        lead[fin.first] = fin.first;
-        size[fin.second] -= size[fin.first];
+        lead[next.f] = next.f;
+        size[next.s] -= size[next.f];
     }
 }
+
 int GetSize(int U) {
     return size[Find(U)];
 }
