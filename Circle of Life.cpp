e2ce742696bb80cfcb3e1373e91024770505419d
Eric Liu <eric__1234@outlook.com>
2017-09-16 13:10:33 -0400

    Update Circle of Life.cpp

--- Circle of Life.cpp
+++ Circle of Life.cpp
@@ -2,18 +2,18 @@
 #define ll long long
 using namespace std;
 int a[100010], b[100010];
-char ch[100010];
-ll N, T, i, j;
+string ch;
+ll N, T, i, j, temp;
 
 int main() {
-	scanf("%lld %lld %s", &N, &T, ch);
-	 for (int i=0; i<N; i++) a[i] = ch[i] - '1';
-	for(int i=63; i>=0; i--) {
-		if(T & ((ll) 1 << i)) {
-			ll temp = ((ll) 1 << i) % N;
+  cin >> N >> T >> ch;
+	for (int i=0; i<N; i++) a[i] = ch[i] - '0';
+	for(int i=59; i>=0; i--) {
+		if((T >> i) & 1) {
+			temp = (1ll << i) % N;
 			for(int j=0; j<N; j++) b[j] = a[(j - temp + N) % N] ^ a[(j + temp) % N];
-			for(j=0; j<N; j++) a[j] = b[j];
+			memcpy(a, b, sizeof a);
 		}
 	}
-	for(int i=0; i<N; i++) printf("%d", (a[i]) ? 1 : 0);
+	for(int i=0; i<N; i++) cout << a[i];
 }
