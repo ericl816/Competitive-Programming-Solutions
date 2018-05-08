5c186e389c1af5c540a586630d7de0cc73a71725
Eric Liu <eric__1234@outlook.com>
2018-05-07 21:25:44 -0400

    Solved tle17c8p5 + Updated C++ Templates

---
+++ C++ Template.cpp
@@ -0,0 +1,32 @@
+#pragma GCC optimize "Ofast"
+#pragma GCC optimize "unroll-loops"
+#pragma GCC target "sse,sse2,sse3,sse4,abm,avx,mmx,popcnt,tune=native"
+// #include <bits/stdc++.h>
+#include "/Users/ericliu/Desktop/Competitive-Programming-Templates/stdc++.h"
+#define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+#define ll long long
+#define MAXM 1010
+#define MAXN 5010
+#define INF 0x3f3f3f3f
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define max(a, b) (a) < (b) ? (b) : (a)
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int, int>
+#define mp make_pair
+#define f first
+#define s second
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+using namespace std;
+
+int main () {
+	// freopen("test.in", "r", stdin);
+	// freopen("test.out", "w", stdout);
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	return 0;
+}
---
+++ Google Code Jam Template.cpp
@@ -0,0 +1,43 @@
+#pragma GCC optimize "Ofast"
+#pragma GCC optimize "unroll-loops"
+#pragma GCC target "sse,sse2,sse3,sse4,abm,avx,mmx,popcnt,tune=native"
+#include <bits/stdc++.h>
+#define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+#define ll long long
+#define MAXN 100010
+#define INF 0x3f3f3f3f
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define max(a, b) (a) < (b) ? (b) : (a)
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int, int>
+#define mp make_pair
+#define f first
+#define s second
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+using namespace std;
+
+// Time Complexity: O(N*T)
+
+int T, N;
+
+int main () {
+	// freopen("test.in", "r", stdin);
+	// freopen("test.out", "w", stdout);
+	scan(T);
+	for (int z=1; z<=T; z++) {
+		scan(N);
+
+		// For outputting
+		printf("%s %d %s", "Case #", z, ": ");
+	}
+	return 0;
+}
+
+/*
+1
+3
+8 9 7
+*/
