4ae02a96c3ffcda606f64091fc70aae5aea68942
Eric Liu <eric__1234@outlook.com>
2018-08-01 21:28:38 -0400

    Solved RGPC '18 P4 - Higgs

---
+++ Higgs.cpp
@@ -0,0 +1,104 @@
+#pragma GCC optimize "Ofast"
+#pragma GCC optimize "unroll-loops"
+#pragma GCC target "sse,sse2,sse3,sse4,abm,avx,mmx,popcnt,tune=native"
+#include <bits/stdc++.h>
+#define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+#define ll long long
+#define MAXN 1000010
+#define INF 0x3f3f3f3f
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define max(a, b) (a) < (b) ? (b) : (a)
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int, int>
+#define mp make_pair
+#define f first
+#define s second
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#ifdef DEBUG
+    #define D(x...) printf(x)
+#else
+    #define D(x...)
+#endif
+using namespace std;
+
+int N, T, a, b, k, S;
+ll ans = INF;
+
+// Can use BIT range updates and queries, or Seg Tree with Lazy Propagation instead
+struct BIT {
+private:
+    int N;
+    vector<ll> bit1, bit2;
+
+public:
+    BIT(int N) : N(N), bit1(N + 1), bit2(N + 1) { }
+
+    void Update (vector<ll> &bit, int idx, int val) {
+        for (; idx<=N; idx += idx & -idx) bit[idx] += val;
+    }
+
+    void Update (int x, int y, int val) {
+        Update(bit1, x, val);
+        Update(bit1, y + 1, -val);
+        Update(bit2, x, val * (x - 1));
+        Update(bit2, y + 1, -val * y);
+    }
+
+    ll Query (vector<ll> &bit, int idx) {
+        ll sum = 0;
+        for (; idx; idx -= idx & -idx) sum += bit[idx];
+        return sum;
+    }
+
+    ll Query (int x) {
+        return Query(bit1, x) * x - Query(bit2, x);
+    }
+
+    ll Query (int x, int y) {
+        return Query(y) - Query(x - 1);
+    }
+};
+
+BIT tree(MAXN);
+
+bool prime[MAXN];
+
+void sieve (int n) {
+    memset(prime, 1, sizeof(prime));
+    prime[1] = 0;
+    for (int i=2; i*i<=n; i++) if (prime[i]) for (int j=i*i; j<=n; j+=i) prime[j] = 0;
+}
+
+int main () {
+    #ifdef NOT_DMOJ
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    #endif // NOT_DMOJ
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N >> T;
+    for (int i=1; i<=N; i++) {
+        cin >> S;
+        tree.Update(i, i, S);
+    }
+    sieve(MAXN);
+    for (int i=1; i<=T; i++) {
+        cin >> a >> b >> k;
+        if (prime[i]) ans = min(ans, (ll) i * (tree.Query(a, b) + k));
+        else tree.Update(a, b, k);
+    }
+    cout << ans << endl;
+    return 0;
+}
+
+/* 
+ * Look for:
+ * the exact constraints (multiple sets are too slow for n=10^6 :( ) 
+ * special cases (n=1?)
+ * overflow (ll vs int?)
+ * array bounds
+ */
